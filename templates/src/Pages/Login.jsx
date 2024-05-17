5c26bca10276812eecc0af411bae67bbb9c6aa8a
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2024-05-17 11:53:36 +0530

    reworking on bucket list app this time using Node express MGdb to build backend --------->>Auth done<<<--------

--- templates/src/Pages/Login.jsx
+++ templates/src/Pages/Login.jsx
@@ -1,107 +1,104 @@
-import React, { useState,useEffect } from 'react';
-import Axios from 'axios'; 
-import {useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [userData, setUserData] = useState(null); // Store fetched user data
   const [alertMessage, setAlertMessage] = useState('');
 
+  // Create the object with the user data
+  const userData = {
+    email: email,
+    password: password
+  };
 
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:4000/users/login', userData);
+      console.log('Response from server:', response.data); // Log the response
+
+      if (response.data.token) {
+        console.log('Login successful, token received:', response.data.token); // Log the token
+        // Navigate to a different page upon successful login
+        navigate('/bucket');
+      } else {
+        console.log('Login failed, response data:', response.data); // Log failure case
+        setAlertMessage(response.data.msg || 'Login failed');
+      }
+    } catch (error) {
+      console.log('Error occurred during login:', error); // Log the error
+      setAlertMessage('An error occurred during login. Please try again.');
+    }
+  }
   const handleAlertClose = () => {
     setAlertMessage('');
   };
-  const handleLogin = async (e) => {
-    e.preventDefault();
-     const filteredUser = userData.find((user) => user.email === email);
 
-     if (filteredUser) {
-       if (filteredUser.password === password) {
-        const userId = filteredUser.id;
-         navigate(`/bucket?action=${encodeURIComponent(userId)}`);
-       } else {
-        setAlertMessage('Incorrect password');
-       }
-     } else {
-      setAlertMessage('User not found');
-     }
-  };
 
-  const fetchUserData = async () => {
-    try {
-      const response = await Axios.get('https://harsh-bucket.onrender.com/users');
-      setUserData(response.data);
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchUserData(); 
-  }, []);
- 
   return (
     <>
-    <div className=" min-h-screen flex items-center justify-center " 
-    style={{ backgroundColor: '#232323'}}
-    >
+      <div className=" min-h-screen flex items-center justify-center "
+        style={{ backgroundColor: '#232323' }}
+      >
 
-      <div className=" p-4 py-16 rounded-lg shadow-lg w-90%  sm:w-96" style={{ backgroundColor: '#232323', boxShadow: "3px 3px #3aa16e, -1em 0 .4em #00a0ff" }}>
-        <h2 className="text-4xl font-semibold text-center mb-6 " style={{ color: '#FFFFFF' }}>Log In</h2>
-        <form>
-          <div className="mb-6">
-            <label className="block font-medium" style={{ color: "#FFFFFF" }}>Email</label>
-            <input
-              type="email"
-              className="w-full border border-gray-300 p-2 rounded-md focus:ring focus:ring-blue-500"
-              placeholder="john@example.com"
-              required
-              value={email}   
-              onChange={(e) => setEmail(e.target.value)}
-              style={{ background: "transparent", borderColor: "transparent", color: "#fff" }}
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block font-medium" style={{ color: "#FFFFFF" }}>Password</label>
-            <input
-              type="password"
-              className="w-full border border-gray-300 p-2 rounded-md focus:ring focus:ring-blue-500"
-              placeholder="********"
-              required
-              value={password}  
-              onChange={(e) => setPassword(e.target.value)}
-              style={{ background: "transparent", borderColor: "transparent", color: "#fff" }}
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-1 rounded-md hover:bg-blue-600 transition duration-300"
-            style={{ background: "transparent", boxShadow: "3px 3px #3aa16e, -0.2em 0 .4em " }}
-            onClick={handleLogin}
-          >
-            Log In
-          </button>
-        </form>
-        <p className="mt-4  text-sm text-center" style={{ color: "#FFFFFF" }}>
-          Don't have an account? <a href="/signup" style={{ color: "#3aa16e" }}
-            
-          >Sign Up</a>
-        </p>
-      </div>
-    </div>
-    {alertMessage && (
-      <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-opacity-50 bg-black z-50">
-        <div className="p-4 rounded-md shadow-lg" style={{backgroundColor:"#666666"}}>
-          <p style={{color:"#fff"}}>{alertMessage}</p>
-          <button onClick={handleAlertClose} className="mt-2 text-white py-1 px-2 rounded-md " style={{backgroundColor:"#4085bf"}}>
-            Close
-          </button>
+        <div className=" p-4 py-16 rounded-lg shadow-lg w-90%  sm:w-96" style={{ backgroundColor: '#232323', boxShadow: "3px 3px #3aa16e, -1em 0 .4em #00a0ff" }}>
+          <h2 className="text-4xl font-semibold text-center mb-6 " style={{ color: '#FFFFFF' }}>Log In</h2>
+          <form>
+            <div className="mb-6">
+              <label className="block font-medium" style={{ color: "#FFFFFF" }}>Email</label>
+              <input
+                type="email"
+                className="w-full border border-gray-300 p-2 rounded-md focus:ring focus:ring-blue-500"
+                placeholder="john@example.com"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ background: "transparent", borderColor: "transparent", color: "#fff" }}
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block font-medium" style={{ color: "#FFFFFF" }}>Password</label>
+              <input
+                type="password"
+                className="w-full border border-gray-300 p-2 rounded-md focus:ring focus:ring-blue-500"
+                placeholder="********"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                style={{ background: "transparent", borderColor: "transparent", color: "#fff" }}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-1 rounded-md hover:bg-blue-600 transition duration-300"
+              style={{ background: "transparent", boxShadow: "3px 3px #3aa16e, -0.2em 0 .4em " }}
+              onClick={handleLogin}
+            >
+              Log In
+            </button>
+          </form>
+          <p className="mt-4  text-sm text-center" style={{ color: "#FFFFFF" }}>
+            Don't have an account? <a href="/signup" style={{ color: "#3aa16e" }}
+
+            >Sign Up</a>
+          </p>
         </div>
       </div>
-    )}
-   </>
+      {alertMessage && (
+        <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-opacity-50 bg-black z-50">
+          <div className="p-4 rounded-md shadow-lg" style={{ backgroundColor: "#666666" }}>
+            <p style={{ color: "#fff" }}>{alertMessage}</p>
+            <button onClick={handleAlertClose} className="mt-2 text-white py-1 px-2 rounded-md " style={{ backgroundColor: "#4085bf" }}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
--- templates/src/Pages/SignUp.jsx
+++ templates/src/Pages/SignUp.jsx
@@ -1,116 +1,3 @@
-// import React,{useState} from 'react';
-// import { useLocation } from 'react-router-dom';
-// import { useNavigate } from 'react-router-dom';
-
-// import axios from 'axios';
-// function SignUp() {
-//   const location = useLocation();
-//   const history = useNavigate();
-//   const searchParams = new URLSearchParams(location.search);
-//   const emailFromParams = searchParams.get('email') || '';
-//   const [formData, setFormData] = useState({
-//     fullName: '',
-//     email: emailFromParams,
-//     password: '',
-//   });
-
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({
-//       ...formData,
-//       [name]: value,
-//     });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     try {
-//       // Send a POST request to your JSON server
-//       const response = await axios.post('http://localhost:8080/users', formData);
-
-//       // Assuming your server responds with the newly created user, you can handle success here.
-//       console.log('User created:', response.data);
-
-//       // Redirect to login page or any other page as needed
-//       history('/login');
-//     } catch (error) {
-//       console.error('Error creating user:', error);
-//       // Handle error here
-//     }
-//   };
-//   return (
-//     <div className=" min-h-screen flex items-center justify-center " style={{ backgroundColor: '#232323' }}>
-//       <div className=" p-8 rounded-lg shadow-lg w-90%  sm:w-96" style={{ backgroundColor: '#232323', boxShadow: "3px 3px #3aa16e, -1em 0 .4em #00a0ff" }}>
-//         <h2 className="text-4xl font-semibold text-center mb-6 " style={{ color: '#FFFFFF' }}>Sign Up</h2>
-//         <form onSubmit={handleSubmit}>
-//           <div className="mb-4">
-//             <label className="block font-medium" style={{ color: "#FFFFFF" }}>Full Name</label>
-//             <input
-//               type="text"
-//               name="fullName"
-//               value={formData.fullName}
-//               onChange={handleChange}
-//               className="w-full border border-green-300 p-2 rounded-md focus:ring focus:ring-blue-500"
-//               placeholder="John Doe"
-//               required
-//               style={{ background: "transparent", borderColor: "transparent", color: "#fff" }}
-//             />
-//           </div>
-//           <div className="mb-4">
-//             <label className="block font-medium" style={{ color: "#FFFFFF" }}>Email</label>
-//             <input
-//               type="email"
-//               name="email"
-//               value={formData.email}
-//               onChange={handleChange}
-//               className="w-full border border-gray-300 p-2 rounded-md focus:ring focus:ring-blue-500"
-//               required
-//               style={{ background: "transparent", borderColor: "transparent", color: "#fff" }}
-//             />
-//           </div>
-//           <div className="mb-6">
-//             <label className="block font-medium" style={{ color: "#FFFFFF" }}>Password</label>
-//             <input
-//               type="password"
-//               name="password"
-//               value={formData.password}
-//               onChange={handleChange}
-//               className="w-full border border-gray-300 p-2 rounded-md focus:ring focus:ring-blue-500"
-//               placeholder="Your ********"
-//               required
-//               style={{ background: "transparent", borderColor: "transparent", color: "#fff" }}
-//             />
-//           </div>
-//           <button
-//             type="submit"
-//             className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
-//             style={{ background: "transparent", boxShadow: "3px 3px #3aa16e, -0.2em 0 .4em " }}
-//           >
-//             Sign Up
-//           </button>
-//         </form>
-//         <p className="mt-4  text-sm text-center" style={{ color: "#FFFFFF" }}>
-//           Already have an account? <a href="/login" style={{ color: "#3aa16e" }}>Log in</a>
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default SignUp;
-
-
-
-
-
-
-
-
-
-
-
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
@@ -125,8 +12,8 @@ function SignUp() {
     fullName: '',
     email: emailFromParams,
     password: '',
-    bucket : [],
-    
+    bucket: [],
+
   });
   const [alertMessage, setAlertMessage] = useState('');
 
@@ -139,34 +26,25 @@ function SignUp() {
   };
   const handleAlertClose = () => {
     setAlertMessage('');
+    history('/login');
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      // Check if the email already exists in the database
-      const existingUser = await axios.get(`https://harsh-bucket.onrender.com/users?email=${formData.email}`);
+    
+      const response = await axios.post('http://localhost:4000/users/register', formData);
 
-      if (existingUser.data.length > 0) {
-        // If the email exists, show a custom alert message
-        setAlertMessage("You can't use the same email to create multiple accounts.");
-      } else {
-        // If the email is not found, proceed to create the user
-        const response = await axios.post('https://harsh-bucket.onrender.com/users', formData);
+      // Assuming your server responds with the newly created user
+      console.log('User created:', response.data);
 
-        // Assuming your server responds with the newly created user, you can handle success here.
-        console.log('User created:', response.data);
+      // Showing a congratulations message
+      setAlertMessage('Congratulations! Your account has been created.');
 
-        // Show a congratulations message
-        setAlertMessage('Congratulations! Your account has been created.');
-
-        // Redirect to login page or any other page as needed
-        history('/login');
-      }
     } catch (error) {
+        // Handle error here
       console.error('Error creating user:', error);
-      // Handle error here
     }
   };
   return (
@@ -226,9 +104,9 @@ function SignUp() {
       </div>
       {alertMessage && (
         <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-opacity-50 bg-black z-50">
-          <div className=" p-4 rounded-md shadow-lg" style={{backgroundColor:"#666666"}}>
-            <p style={{color:"#fff"}}>{alertMessage}</p>
-            <button onClick={handleAlertClose}  className="mt-2 text-white py-1 px-2 rounded-md " style={{backgroundColor:"#4085bf"}}>
+          <div className=" p-4 rounded-md shadow-lg" style={{ backgroundColor: "#666666" }}>
+            <p style={{ color: "#fff" }}>{alertMessage}</p>
+            <button onClick={handleAlertClose} className="mt-2 text-white py-1 px-2 rounded-md " style={{ backgroundColor: "#4085bf" }}>
               Close
             </button>
           </div>
@@ -240,98 +118,3 @@ function SignUp() {
 
 export default SignUp;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "bucket": {
-//   "Travel": [
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     },
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     }
-//   ],
-//   "Friends and Family": [
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     },
-//     {
-//       "title": "Travel item 2",
-//       "createdAt": "2023-10-02T09:15:00",
-//       "targetDate": null
-//     }
-//   ],
-//   "Love": [
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     },
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     }
-//   ],
-//   "Entertainment": [
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     },
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     }
-//   ],
-//   "Self Improvement": [
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     },
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     }
-//   ],
-//   "Taste & Treats": [
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     },
-//     {
-//       "title": "",
-//       "createdAt": "",
-//       "targetDate": null
-//     }
-//   ]
-// }
